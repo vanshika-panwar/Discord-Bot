464e901208ffafde08388a2f559f47dbce23edb3
vanshika-panwar <201851140@iiitvadodara.ac.in>
2021-03-12 16:56:08 +0530

    second

--- bot.py
+++ bot.py
@@ -13,19 +13,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
-# bot = commands.Bot(command_prefix='!')
-# @bot.command(name='create-channel')
-
-# @commands.has_role('admin')
-# async def create_channel(ctx, channel_name='real-python'):
-#     guild = ctx.guild
-#     existing_channel = discord.utils.get(guild.channels, name=channel_name)
-#     if not existing_channel:
-#         print(f'Creating a new channel: {channel_name}')
-#         await guild.create_text_channel(channel_name)
-
-# @bot.event
-
 sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
 starter_encouragements = [
   "Cheer up!",
